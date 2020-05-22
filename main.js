fdb9d9ec9369219d98197f0c8e7023b47763231f
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-22 14:06:58 +0200

    click sul button, lettura del testo utente, inizio funzione ajax

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+//MILESTONE 1
+
+//intercettare il clik sul bottone di ricerca_utente
+$('#bottone').click(function(){
+//leggere il testo scritto dall'ricerca_utente
+    var testo_utente = $('#ricerca_utente').val().trim().toLowerCase();
+    console.log(testo_utente);
+//chiamata funzione ajax
+    $.ajax({
+        'url': 'https://api.themoviedb.org/3/search/movie',
+        'method': 'GET',
+        'data': {
+            'api_key': '6fb6f7fed203f8fd60f7a424fa51f6e5',
+            'query': testo_utente,
+            'language': 'it'
+        },
+        'success': function(){
+            //istruzioni per recuperare i dati
+
+        },
+        'error': function() {
+            alert('Si è verificato un errore')
+        }
+
+    });
+
+});
+
+});
