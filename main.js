1cc45f64ce0ca8065aa1340b55480ca4d97ff261
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-22 17:13:41 +0200

    eliminare la ricerca precedente

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ $('#bottone').click(function(){
 //leggere il testo scritto dall'ricerca_utente
     var testo_utente = $('#ricerca_utente').val().trim().toLowerCase();
     console.log(testo_utente);
+    $('#ricerca_utente').focus(function(){
+        $('main').html('');
+    });
 //chiamata funzione ajax
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
@@ -45,10 +48,8 @@ $('#bottone').click(function(){
 
             //stampare in pagina titolo, titolo originale, lingua originale, voto
             $('main').append('<ul class="lista-proprietà-film"><li class="titolo-film">' + film_corrente.title +'</li><li class="titolo-originale">' + film_corrente.original_title  + '</li><li class"lingua">' + film_corrente.original_language +'</li><li class="voto-average">' + film_corrente.vote_average + '</li></ul>');
-            testo_utente = $('#ricerca_utente').val('');
-            $('#ricerca_utente').focus(function(){
-                $('main').html('');
-            });
+
+
             }
 
         },
