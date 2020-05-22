6166a01ec7fbce5e8173c0a8ac725c40ffc957e9
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-22 17:50:04 +0200

    creare funzioni

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ $('#bottone').click(function(){
 //leggere il testo scritto dall'ricerca_utente
     var testo_utente = $('#ricerca_utente').val().trim().toLowerCase();
     console.log(testo_utente);
-    $('#ricerca_utente').focus(function(){
-        $('main').html('');
-    });
+
+    $('main').html('');
+
 //chiamata funzione ajax
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
@@ -22,21 +22,15 @@ $('#bottone').click(function(){
         'success': function(data){
             // recuperare l'array di film
             var array_film = data.results;
-            console.log(array_film); //è un array di oggetti
-
-            //recuperare il singolo film dalla lista
+            console.log(array_film);
+            //ciclo l'array per recuperare ogni singolo elemento, ogni elemento è un oggetto
             for(var i = 0; i < array_film.length; i++) {
-                var film_corrente = array_film[i];
+                // recupero ogni singolo film dalla lista
+                var film_corrente= array_film[i];
                 console.log(film_corrente);
-            //RECUPERARE LE PROPRIETA' DELL'OGGETTO DA STAMPARE IN PAGINA: TITOLO, TITOLO ORIGINALE, LINGUA, VOTO
-                // var titolo = film_corrente.title;
-                // var titolo_originale = film_corrente.original_title;
-                // var lingua = film_corrente.original_language;
-                // var voto = film_corrente.vote_average;
-                // console.log(titolo);
-                // console.log(titolo_originale);
-                // console.log(lingua);
-                // console.log(voto);
+
+
+
             //creo un nuovo oggetto in cui salvo le proprietà che voglio quindi elimino le variabili create precedentemente
             var nuovo_oggetto = {
                 'titolo':film_corrente.title,
@@ -53,6 +47,7 @@ $('#bottone').click(function(){
             }
 
         },
+
         'error': function() {
             alert('Si è verificato un errore')
         }
@@ -61,7 +56,11 @@ $('#bottone').click(function(){
 
 });
 
+
 });
+
+
+
 
 
 //ricorda che data riga14 è diverso da data riga19
