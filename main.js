3e81384748080b47318f6bf10b71418429d2bd13
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-23 15:31:03 +0200

    button disabilitato se l'input è vuoto

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ $('#bottone').click(function(){
 //leggere il testo scritto dall'ricerca_utente
     var testo_utente = $('#ricerca_utente').val().trim().toLowerCase();
     console.log(testo_utente);
-
+//ogni nuovo input dell'utente sono eliminati i dati della ricerca precedente
     $('main').html('');
-
+//quando l'input è vuoto il button è disabilitato
+    if (testo_utente == '') {
+        $(button).prop("disabled", true);
+        console.log(button)
+    }
 //chiamata funzione ajax
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
@@ -28,9 +32,6 @@ $('#bottone').click(function(){
                 // recupero ogni singolo film dalla lista
                 var film_corrente= array_film[i];
                 console.log(film_corrente);
-
-
-
             //creo un nuovo oggetto in cui salvo le proprietà che voglio quindi elimino le variabili create precedentemente
             var nuovo_oggetto = {
                 'titolo':film_corrente.title,
@@ -43,22 +44,15 @@ $('#bottone').click(function(){
             //stampare in pagina titolo, titolo originale, lingua originale, voto
             $('main').append('<ul class="lista-proprietà-film"><li class="titolo-film">' + film_corrente.title +'</li><li class="titolo-originale">' + film_corrente.original_title  + '</li><li class"lingua">' + film_corrente.original_language +'</li><li class="voto-average">' + film_corrente.vote_average + '</li></ul>');
 
-
             }
-
         },
-
         'error': function() {
             alert('Si è verificato un errore')
         }
-
     });
-
 });
 
-
 });
-
 
 
 
