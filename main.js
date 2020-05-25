b45298627df64a73934126487d9a7872393f4302
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-25 12:03:27 +0200

    template handlebars

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
 
+var template_html = $('#entry-template').html();
+var template_function = Handlebars.compile(template_html);
 //MILESTONE 1
 
 //intercettare il clik sul bottone di ricerca_utente
 $('#bottone').click(function(){
+
 //leggere il testo scritto dall'ricerca_utente
     var testo_utente = $('#ricerca_utente').val().trim().toLowerCase();
     console.log(testo_utente);
+//la chiamata ajax parte qunado il testo utente non è una stringa vuota
+    if (testo_utente != '') {
+//svutare l'input
+    $('#ricerca_utente').val('');
 //ogni nuovo input dell'utente sono eliminati i dati della ricerca precedente
-    $('main').html('');
+    $('.container').html('');
 //chiamata funzione ajax
+
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
         'method': 'GET',
@@ -35,15 +43,19 @@ $('#bottone').click(function(){
                 'voto':film_corrente.vote_average
             }
             console.log(nuovo_oggetto);
-            //stampare in pagina titolo, titolo originale, lingua originale, voto
-            $('main').append('<ul class="lista-proprietà-film"><li class="titolo-film">' + film_corrente.title +'</li><li class="titolo-originale">' + film_corrente.original_title  + '</li><li class"lingua">' + film_corrente.original_language +'</li><li class="voto-average">' + film_corrente.vote_average + '</li></ul>');
-
+            //stampare in pagina titolo, titolo originale, lingua originale, voto con il template
+            // $('main').append('<ul class="lista-proprietà-film"><li class="titolo">' + film_corrente.title +'</li><li class="titolo-originale">' + film_corrente.original_title  + '</li><li class"lingua">' + film_corrente.original_language +'</li><li class="voto-average">' + film_corrente.vote_average + '</li></ul>');
+            var html_finale = template_function(nuovo_oggetto);
+            $('.container').append(html_finale);
             }
         },
         'error': function() {
             alert('Si è verificato un errore')
         }
     });
+} else {
+    alert('Non hai effettuato nessuna ricerca') //alert per l'utente che non ha
+}
 });
 });
 
